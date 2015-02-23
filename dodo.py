0928393c4c0ec4d985dce7657bc0aa7dd2729c7d
Scott A. Idler <scott.a.idler@intel.com>
2015-02-22 23:14:38 -0800

    removed file_dep and fixed func call to index ... -sai

--- dodo.py
+++ dodo.py
@@ -7,10 +7,9 @@ from subprocess import check_output, PIPE
 
 def task_updateinit():
     output = check_output('git submodule', shell=True)
-    submods = [ line.split()(1) for line in output.strip().split('\n') ]
+    submods = [ line.split()[1] for line in output.strip().split('\n') ]
     for submod in submods:
         yield {
             'name': submod,
-            'file_dep': submod,
             'actions': ['git submodule update --init %(submod)s' % locals() ],
         }
