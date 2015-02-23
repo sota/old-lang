25ca06b3b7b1d1808176ebc4452e3a3cf395e747
Scott A. Idler <scott.a.idler@intel.com>
2015-02-22 23:07:57 -0800

    refactored updateinit to run in parallel... -sai

--- dodo.py
+++ dodo.py
@@ -3,20 +3,14 @@
 import sys
 sys.dont_write_bytecode = True
 
-def task_hello():
-    """hello py """
-
-    def python_hello(times, text, targets):
-        with open(targets[0], "a") as output:
-            output.write(times * text)
-
-    return {
-        'actions': [(python_hello, [3, "py!\n"])],
-        'targets': ["hello.txt"],
-    }
+from subprocess import check_output, PIPE
 
 def task_updateinit():
-    return {
-        'actions': ['git submodule update --init repos/*'],
-        'targets': ['repos/pypy', 'repos/ragel'],
-    } 
+    output = check_output('git submodule', shell=True)
+    submods = [ line.split()(1) for line in output.strip().split('\n') ]
+    for submod in submods:
+        yield {
+            'name': submod,
+            'file_dep': submod,
+            'actions': ['git submodule update --init %(submod)s' % locals() ],
+        }
