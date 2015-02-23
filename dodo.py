919503b810a457489c05171fe39cb66f8e784dc6
Scott A. Idler <scott.a.idler@intel.com>
2015-02-23 00:31:57 -0800

    refactored; added pyflakes ... -sai

--- dodo.py
+++ dodo.py
@@ -2,8 +2,25 @@
 
 import sys
 sys.dont_write_bytecode = True
-
+import inspect
 from subprocess import check_output, check_call
+
+sotasrc = 'targetsota.py'
+python = 'python' if check_call('which pypy', shell=True) else 'pypy'
+python = 'python' # FIXME:  its slower; doing this for now ... -sai
+rpython = 'repos/pypy/rpython/bin/rpython'
+
+def env():
+    try:
+        frame = inspect.currentframe().f_back
+        return dict(frame.f_globals.items() + frame.f_locals.items() )
+    finally:
+        del frame
+
+def task_pyflakes():
+    return {
+        'actions': ['pyflakes %(sotasrc)s' % env() ],
+    }
 
 def task_submod_update():
     output = check_output('git submodule', shell=True)
@@ -11,17 +28,13 @@ def task_submod_update():
     for submod in submods:
         yield {
             'name': submod,
-            'actions': ['git submodule update --init %(submod)s' % locals() ],
+            'actions': ['git submodule update --init %(submod)s' % env() ],
             'targets': [submod]
         }
 
 def task_build_sota():
-    python = 'python' if check_call('which pypy', shell=True) else 'pypy'
-    python = 'python' # FIXME:  its slower; doing this for now ... -sai
-    sotasrc = 'targetsota.py'
-    rpython = 'repos/pypy/rpython/bin/rpython'
     return {
-        'actions': ['%(python)s %(rpython)s %(sotasrc)s' % locals() ],
+        'actions': ['%(python)s %(rpython)s %(sotasrc)s' % env() ],
         'file_dep': [rpython],
         'targets': ['targetsota-c'],
     }
