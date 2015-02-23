5076b6c5aedc3f1184d8faee3f9a8d9760cfde0e
Scott A. Idler <scott.a.idler@intel.com>
2015-02-22 23:25:57 -0800

    added task build_sota ... -sai

--- dodo.py
+++ dodo.py
@@ -5,11 +5,19 @@ sys.dont_write_bytecode = True
 
 from subprocess import check_output, PIPE
 
-def task_updateinit():
+def task_submod_update():
     output = check_output('git submodule', shell=True)
     submods = [ line.split()[1] for line in output.strip().split('\n') ]
     for submod in submods:
         yield {
             'name': submod,
             'actions': ['git submodule update --init %(submod)s' % locals() ],
+            'targets': [submod]
         }
+
+def task_build_sota():
+    return {
+        'actions': ['python repos/pypy/rpython/bin/rpython targetsota.py'],
+        'file_dep': ['repos/pypy/rpython/bin/rpython'],
+        'targets': ['targetsota-c'],
+    }
