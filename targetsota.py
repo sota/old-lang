2e147f32b914df0defbda17fa7af95ea9269d264
Scott A. Idler <scott.a.idler@intel.com>
2015-03-25 00:03:54 -0700

    added rffi example with foo function... -sai

--- targetsota.py
+++ targetsota.py
@@ -3,6 +3,15 @@ sota: State of the Art
 
 The target below specifies the sota dynamic programming language.
 '''
+import os
+lexer_dir = os.path.join(os.getcwd(), 'src/lexer')
+lexer_h = 'lexer.h'
+liblexer_a = 'lexer'
+
+from rpython.rtyper.lltypesystem import rffi, lltype
+from rpython.translator.tool.cbuild import ExternalCompilationInfo
+eci = ExternalCompilationInfo(include_dirs=[lexer_dir], includes=[lexer_h], library_dirs=[lexer_dir], libraries=[liblexer_a])
+foo = rffi.llexternal('foo', [], lltype.Void, compilation_info=eci)
 
 def debug(msg):
     print 'debug:', msg
@@ -10,6 +19,7 @@ def debug(msg):
 # __________  Entry point  __________
 
 def entry_point(argv):
+    foo()
     debug('sota')
     return 0
 
