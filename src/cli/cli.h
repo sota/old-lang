241dffa929f102c3bcdd800ec3eb9df91dfa4e34
scottidler <scottidler@gmail.com>
2015-04-12 14:19:06 -0700

    changed from g++ -> gcc ... -sai

--- src/cli/cli.h
+++ src/cli/cli.h
@@ -1,24 +1,12 @@
 #ifndef __SOTA_CLI__
 #define __SOTA_CLI__ = 1
 
-#include <array>
-#include <string>
 #include <stdio.h>
-#include <fstream>
-#include <iostream>
-#include <exception>
-#include <sys/stat.h>
 
-#include "tclap/CmdLine.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
+void foo();
+int bar(int x);
+int baz(int x, const char *text);
 
 int parse(int argc, char **argv);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /*__SOTA_CLI__*/
