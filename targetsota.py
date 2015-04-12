39da0b6acd6c69ba739f45903dd74488837757f4
scottidler <scottidler@gmail.com>
2015-04-12 14:22:49 -0700

    cleaned up targetsota with some examples of calling foreign funcs; all but parse work ... -sai

--- targetsota.py
+++ targetsota.py
@@ -4,14 +4,39 @@ sota: State of the Art
 The target below specifies the sota dynamic programming language.
 '''
 import os
-lexer_dir = os.path.join(os.getcwd(), 'src/lexer')
-lexer_h = 'lexer.h'
-liblexer_a = 'lexer'
 
 from rpython.rtyper.lltypesystem import rffi, lltype
 from rpython.translator.tool.cbuild import ExternalCompilationInfo
-eci = ExternalCompilationInfo(include_dirs=[lexer_dir], includes=[lexer_h], library_dirs=[lexer_dir], libraries=[liblexer_a])
-foo = rffi.llexternal('foo', [], lltype.Void, compilation_info=eci)
+cli_dir = os.path.join(os.getcwd(), 'src/cli')
+cli_eci = ExternalCompilationInfo(
+    include_dirs=[cli_dir],
+    includes=['cli.h'],
+    library_dirs=[cli_dir],
+    libraries=['cli'])
+
+foo = rffi.llexternal(
+    'foo',
+    [],
+    lltype.Void,
+    compilation_info=cli_eci)
+
+bar = rffi.llexternal(
+    'bar',
+    [lltype.Signed],
+    lltype.Signed,
+    compilation_info=cli_eci)
+
+baz = rffi.llexternal(
+    'baz',
+    [lltype.Signed, rffi.CCHARP],
+    lltype.Signed,
+    compilation_info=cli_eci)
+
+parse = rffi.llexternal(
+    'parse',
+    [lltype.Signed,lltype.Ptr(lltype.Array(rffi.CCHARP, hints={'nolength': True}))],
+    lltype.Signed,
+    compilation_info=cli_eci)
 
 def debug(msg):
     print 'debug:', msg
@@ -19,7 +44,27 @@ def debug(msg):
 # __________  Entry point  __________
 
 def entry_point(argv):
+    print 'argv =', argv
+
+    args = ' '.join(argv[1:])
+    args = ''
+    for arg in argv:
+        args += ' '
+        if ' ' in arg:
+            args += '"' + arg + '"'
+        else:
+            args += arg
+    print 'args =', args
+    if isinstance(argv, list):
+        print 'isinstance of list'
+    if len(argv) and isinstance(argv[0], str):
+        print 'argv[0] isinstance of str'
+
     foo()
+    print 'bar_result =', bar(13)
+    print 'baz_result =', baz(-13, "donkeypunch")
+    parse(2, ["donkey", "punch"])
+
     debug('sota')
     return 0
 
