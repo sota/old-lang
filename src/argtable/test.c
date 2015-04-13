cfe153a741b79ab453d3728d6a024d7e49f50cd3
scottidler <scottidler@gmail.com>
2015-04-12 23:14:32 -0700

    made small example... -sai

--- src/argtable/test.c
+++ src/argtable/test.c
@@ -1,5 +1,13 @@
-#include "argtable3.h"
+#include "argtable/argtable3.h"
 
 int main(int argc, char **argv) {
+    struct arg_lit *help = arg_lit0(NULL, "help", "print this help and exit");
+    struct arg_end *end     = arg_end(20);
+    void *argtable[] = {
+        help,
+        end,
+    };
+    arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+    printf("test\n");
     return 0;
 }
