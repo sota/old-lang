74373aa888f3c7df344ba53ab54cf460937ceeee
scottidler <scottidler@gmail.com>
2015-04-13 20:44:06 -0700

    added scan rffi call... -sai

--- targetsota.py
+++ targetsota.py
@@ -7,6 +7,7 @@ import os
 
 from rpython.rtyper.lltypesystem import rffi, lltype
 from rpython.translator.tool.cbuild import ExternalCompilationInfo
+
 cli_dir = os.path.join(os.getcwd(), 'src/cli')
 cli_eci = ExternalCompilationInfo(
     include_dirs=[cli_dir],
@@ -20,6 +21,19 @@ parse = rffi.llexternal(
     lltype.Signed,
     compilation_info=cli_eci)
 
+lexer_dir = os.path.join(os.getcwd(), 'src/lexer')
+lexer_eci = ExternalCompilationInfo(
+    include_dirs=[lexer_dir],
+    includes =['lexer.h'],
+    library_dirs=[lexer_dir],
+    libraries=['lexer'])
+
+scan = rffi.llexternal(
+    'scan',
+    [rffi.CCHARP],
+    lltype.Signed,
+    compilation_info=lexer_eci)
+
 def debug(msg):
     print 'debug:', msg
 
@@ -28,12 +42,12 @@ def debug(msg):
 def entry_point(argv):
     argv_charpp = rffi.liststr2charpp(argv)
     try:
-        parse(len(argv), argv_charpp)
+        exitcode = parse(len(argv), argv_charpp)
+        #result = scan("sota baby")
     finally:
         rffi.free_charpp(argv_charpp)
 
-    debug('sota')
-    return 0
+    return exitcode
 
 # _____ Define and setup target ___
 
