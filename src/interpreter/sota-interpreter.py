4bdddaa34350d430a5dcd8d106cd78f1d51828b7
scottidler <scottidler@gmail.com>
2015-04-19 20:48:08 -0700

    wired to try Point and foo example from lexer... -sai

--- src/interpreter/sota-interpreter.py
+++ src/interpreter/sota-interpreter.py
@@ -4,35 +4,40 @@ sota: State of the Art
 The target below specifies the sota dynamic programming language.
 '''
 import os
-#
-#from rpython.rtyper.lltypesystem import rffi, lltype
-#from rpython.translator.tool.cbuild import ExternalCompilationInfo
-#
-#cli_dir = os.path.join(os.getcwd(), 'src/cli')
-#cli_eci = ExternalCompilationInfo(
-#    include_dirs=[cli_dir],
-#    includes=['cli.h'],
-#    library_dirs=[cli_dir],
-#    libraries=['cli'])
-#
-#parse = rffi.llexternal(
-#    'parse',
-#    [lltype.Signed, rffi.CCHARPP],
-#    lltype.Signed,
-#    compilation_info=cli_eci)
-#
-#lexer_dir = os.path.join(os.getcwd(), 'src/lexer')
-#lexer_eci = ExternalCompilationInfo(
-#    include_dirs=[lexer_dir],
-#    includes =['lexer.h'],
-#    library_dirs=[lexer_dir],
-#    libraries=['lexer'])
-#
-#scan = rffi.llexternal(
-#    'scan',
-#    [rffi.CCHARP],
-#    lltype.Signed,
-#    compilation_info=lexer_eci)
+
+from rpython.rtyper.lltypesystem import rffi, lltype
+from rpython.translator.tool.cbuild import ExternalCompilationInfo
+
+lexer_dir = os.path.join(os.getcwd(), 'src/lexer')
+lexer_eci = ExternalCompilationInfo(
+    include_dirs=[lexer_dir],
+    includes=['lexer.h'],
+    library_dirs=[lexer_dir],
+    libraries=['lexer'])
+
+SotaToken = lltype.Struct(
+    'SotaToken',
+    ('name', rffi.CCHARP),
+    ('value', rffi.CCHARP),
+    ('line', rffi.LONG),
+    ('pos', rffi.LONG))
+
+Point = lltype.GcStruct(
+    'Point',
+    ('x', lltype.Signed),
+    ('y', lltype.Signed))
+
+foo = rffi.llexternal(
+    'foo',
+    [lltype.Ptr(Point)],
+    lltype.Signed,
+    compilation_info=lexer_eci)
+
+scan = rffi.llexternal(
+    'scan',
+    [rffi.CCHARP, lltype.Ptr(SotaToken)],
+    rffi.INT,
+    compilation_info=lexer_eci)
 
 def loadfile(src):
     return open(src).read()
@@ -43,6 +48,17 @@ def debug(msg):
 # __________  Entry point  __________
 
 def entry_point(argv):
+
+    print 'lexer_dir =', lexer_dir
+    tokens = lltype.Ptr(SotaToken)
+    pointp = lltype.nullptr(Point)
+    r = foo(pointp)
+    print lltype.typeOf(r)
+    print lltype.typeOf(pointp)
+    print lltype.typeOf(pointp.x)
+    z = pointp.x + pointp.y
+    print z
+
     exitcode = 0
     if len(argv) == 2:
         filename, src = argv
