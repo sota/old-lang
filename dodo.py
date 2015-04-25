299ffedbbbf80bc85aaf1918a8757195d4e5b884
scottidler <scottidler@gmail.com>
2015-04-25 09:54:01 -0700

    abstracted submods to be used in task_submod and task_tidy... -sai

--- dodo.py
+++ dodo.py
@@ -7,7 +7,7 @@ sys.dont_write_bytecode = True
 
 SCRIPT_PATH, BASENAME = os.path.split(os.path.realpath(__file__) )
 SCRIPT_NAME, SCRIPT_EXT = os.path.splitext(os.path.basename(BASENAME) )
-sys.path.insert(0, os.path.abspath(os.path.join(SCRIPT_PATH, 'utils') ) )
+sys.path.insert(0, os.path.abspath(os.path.join(SCRIPT_PATH, 'src/utils') ) )
 
 from common import cd, call, env
 from doit.task import clean_targets
@@ -20,7 +20,7 @@ ragel = 'src/ragel/ragel/ragel'
 targetdir = 'src/sota-jit'
 targetsrc = 'sota-jit.py'
 sotadir = 'src/sota'
-sotasrc = 'sota.c'
+sotasrc = 'sota.cpp'
 sotajit = 'sota-jit'
 python = 'python' if call('which pypy', throw=False)[0] else 'pypy'
 python = 'python' # FIXME:  its slower; doing this for now ... -sai
@@ -29,6 +29,10 @@ rpython = 'src/pypy/rpython/bin/rpython'
 PRE = 'tests/pre'
 POST = 'tests/post'
 
+def submods():
+    stdout = call('git submodule')[1]
+    return [line.split()[1] for line in stdout.strip().split('\n')]
+
 def task_pyflakes():
     return {
         'actions': ['pyflakes %(targetsrc)s' % env() ],
@@ -36,9 +40,7 @@ def task_pyflakes():
     }
 
 def task_submod():
-    stdout = call('git submodule')[1]
-    submods = [line.split()[1] for line in stdout.strip().split('\n')]
-    for submod in submods:
+    for submod in submods():
         yield {
             'name': submod,
             'verbosity': 2,
@@ -64,7 +66,7 @@ def task_ccode():
             ragel,
             'src/lexer/lexer.h',
             'src/lexer/lexer.rl',
-            'src/sota/sota.c',
+            'src/sota/sota.cpp',
         ],
         'actions': ['cd src && tup', 'cp -f src/sota/sota .'],
         'targets': ['src/lexer/liblexer.a', 'sota'],
@@ -115,21 +117,23 @@ def task_post():
 
 def task_success():
     return {
-        'verbosity': 2,
         'file_dep': ['%(POST)s/results' % env()],
         'actions': [
-            '%(sotadir)s/sota' % env(),
+            '%(sotadir)s/sota --help' % env(),
             'echo "sota build success!"',
         ],
     }
 
 def task_tidy():
-    return {
+
+    for submod in submods():
+        yield {
+            'name': submod,
+            'verbosity': 2,
+            'actions': ['cd %(submod)s && git reset --hard HEAD && git clean -xfd' % env()],
+        }
+    yield {
+        'name': 'sota/lang',
         'verbosity': 2,
-        'actions': [
-            'git clean -xfd',
-            'cd src/pypy && git reset --hard HEAD && git clean -xfd',
-            'cd src/ragel && git reset --hard HEAD && git clean -xfd',
-            'cd src/argtable3 && git reset --hard HEAD && git clean -xfd',
-        ],
+        'actions': ['git clean -xfd'],
     }
