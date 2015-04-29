fed70c0adf0f7164515461939e15449ee948215c
scottidler <scottidler@gmail.com>
2015-04-29 10:32:43 -0700

    wired call to libcli.a into sota target... -sai

--- src/jit/sota.py
+++ src/jit/sota.py
@@ -10,6 +10,14 @@ from rpython.rtyper.lltypesystem import rffi, lltype
 from rpython.rtyper.tool import rffi_platform as platform
 from rpython.translator.tool.cbuild import ExternalCompilationInfo
 
+cli_dir = os.path.join(os.getcwd(), 'src/cli')
+cli_eci = ExternalCompilationInfo(
+    include_dirs=[cli_dir],
+    includes=['cli.h'],
+    library_dirs=[cli_dir],
+    libraries=['cli'],
+    use_cpp_linker=True)
+
 lexer_dir = os.path.join(os.getcwd(), 'src/lexer')
 lexer_eci = ExternalCompilationInfo(
     include_dirs=[lexer_dir],
@@ -55,6 +63,19 @@ scan = rffi.llexternal(
     rffi.INT,
     compilation_info=lexer_eci)
 
+CLITOKEN = rffi.CStruct(
+    'CliToken',
+    ('name', rffi.CCHARP),
+    ('value', rffi.CCHARP))
+CLITOKENP = rffi.CArrayPtr(CLITOKEN)
+CLITOKENPP = rffi.CArrayPtr(CLITOKENP)
+
+parse = rffi.llexternal(
+    'parse',
+    [rffi.LONG, rffi.CCHARPP, CLITOKENPP],
+    rffi.LONG,
+    compilation_info=cli_eci)
+
 def loadfile(src):
     return open(src).read()
 
@@ -72,6 +93,13 @@ def entry_point(argv):
         print 'source is file'
     else:
         print 'source is text'
+
+    with lltype.scoped_alloc(CLITOKENPP.TO, 1) as clitokenpp:
+        result = parse(len(argv), rffi.liststr2charpp(argv), clitokenpp)
+        for i in range(result):
+            clitoken = clitokenpp[0][i]
+            print 'CliToken {name=%s, value=%s}' % (rffi.charp2str(clitoken.c_name), rffi.charp2str(clitoken.c_value))
+        print 'result =', result
 
 #    with lltype.scoped_alloc(CPOINTPP.TO, 1) as cpointpp:
 #        count = foo(cpointpp)
