5871c815be7c7c6a04045822172bc9a233f91aae
Scott A. Idler <scott.a.idler@intel.com>
2015-04-30 23:53:33 -0700

    removed malloc code in favor of tclap stub... -sai

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -8,18 +8,7 @@
 #include <stdio.h>
 
 extern "C" int parse(int argc, char *argv[], struct CliToken **tokens) {
-    printf("parse\n");
-    int count = 3;
-    *tokens = (struct CliToken *)malloc(count * sizeof(struct CliToken));
-    for (int i=0; i<count; ++i) {
-        (*tokens)[i].name = "si";
-        (*tokens)[i].value = "og";
-    }
-    return count;
-}
-/*
-int parse(int argc, char *argv[]) {
-    printf("cli::parse\n");
+    int result = 0;
     try {
         TCLAP::CmdLine cmdline("sota: state of the art", ' ', "0.1");
         TCLAP::UnlabeledValueArg<std::string> sourceArg(
@@ -37,14 +26,9 @@ int parse(int argc, char *argv[]) {
             std::cerr << "source must be non-empty text or file" << source << std::endl;
             return 1;
         }
-        std::ostringstream cmd;
-        cmd << "./sota-jit \"" << source << "\"";
-        system(cmd.str().c_str());
 
     } catch (TCLAP::ArgException &ae) {
         std::cerr << "error: " << ae.error() << " for arg " << ae.argId() << std::endl;
     }
-    return 0;
+    return result;
 }
-*/
-
