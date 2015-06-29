4e53d3305f9d340ccf2be094b9a833c07fbf110e
scottidler <scottidler@gmail.com>
2015-06-28 17:45:23 -0700

    added naval fate example to cli.cpp... -sai

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -1,15 +1,19 @@
 #include "cli.h"
 
+#include <stdio.h>
+#include <map>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
 #include <algorithm>
 #include "tclap/CmdLine.h"
-#include <stdio.h>
+
+#include "docopt.cpp/docopt.h"
 
 #include "../version.h"
 
+/*
 extern "C" int parse(int argc, char *argv[], struct CliToken **tokens) {
     int result = 0;
     try {
@@ -32,6 +36,36 @@ extern "C" int parse(int argc, char *argv[], struct CliToken **tokens) {
 
     } catch (TCLAP::ArgException &ae) {
         std::cerr << "error: " << ae.error() << " for arg " << ae.argId() << std::endl;
+    }
+    return result;
+}
+*/
+
+static const char USAGE[] =
+R"(Naval Fate.
+
+    Usage:
+      naval_fate ship new <name>...
+      naval_fate ship <name> move <x> <y> [--speed=<kn>]
+      naval_fate ship shoot <x> <y>
+      naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
+      naval_fate (-h | --help)
+      naval_fate --version
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+      --speed=<kn>  Speed in knots [default: 10].
+      --moored      Moored (anchored) mine.
+      --drifting    Drifting mine.
+)";
+
+extern "C" int parse(int argc, char *argv[], struct CliToken **tokens) {
+    int result = 0;
+    std::map<std::string, docopt::value> args = docopt::docopt(USAGE, {argv+1, argv+argc}, true, "Naval Fate 2.0");
+
+    for(auto const& arg : args) {
+        std::cout << arg.first <<  arg.second << std::endl;
     }
     return result;
 }
