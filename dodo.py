4fe94fd4985e22949e867de71b683a2988b5313f
scottidler <scottidler@gmail.com>
2015-07-04 10:44:21 -0700

    moved SotaVersionUpdater (svu) to utils/__init__.py... -sai

--- dodo.py
+++ dodo.py
@@ -53,26 +53,6 @@ except:
     except:
         VERSION = 'UNKNOWN'
 
-class SotaVersionUpdater(object):
-    def __init__(self, filename, version):
-        self.filename = filename
-        self.version = version
-        self.pattern = '''(.*SOTA_VERSION += +["'])([\w\-\.]+)(["'])'''
-        self.regex = re.compile(self.pattern)
-        self.replace = r'\1' + version + r'\3'
-        self.contents = open(filename).read()
-    def uptodate(self):
-        match = self.regex.search(self.contents)
-        if match:
-            return match.group(2) == self.version
-        return False
-    def update(self):
-        updated = self.regex.sub(self.replace, self.contents)
-        if not updated:
-            raise Exception('VersionUpdater.update produced empty updated string')
-        with open(self.filename, 'w') as f:
-            f.write(updated)
-
 def task_version():
     for filename in [versionh, versionpy]:
         svu = SotaVersionUpdater(filename, VERSION)
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -220,3 +220,24 @@ def merge(a, b):
         except TypeError, e:
             raise MergeError('TypeError "%s" in key "%s" when merging "%s" into "%s"' % (e, key, b, a))
     return a
+
+class SotaVersionUpdater(object):
+    def __init__(self, filename, version):
+        self.filename = filename
+        self.version = version
+        self.pattern = '''(.*SOTA_VERSION += +["'])([\w\-\.]+)(["'])'''
+        self.regex = re.compile(self.pattern)
+        self.replace = r'\1' + version + r'\3'
+        self.contents = open(filename).read()
+    def uptodate(self):
+        match = self.regex.search(self.contents)
+        if match:
+            return match.group(2) == self.version
+        return False
+    def update(self):
+        updated = self.regex.sub(self.replace, self.contents)
+        if not updated:
+            raise Exception('VersionUpdater.update produced empty updated string')
+        with open(self.filename, 'w') as f:
+            f.write(updated)
+
