3bbffa9702b88f811838aa6e1d262f01f5b18f32
scott.a.idler <scott.a.idler@gmail.com>
2016-03-09 00:45:49 -0800

    ordered dict and hashing funcs... -sai

--- src/sast/expressions.py
+++ src/sast/expressions.py
@@ -5,10 +5,23 @@ from rpython.rlib.objectmodel import import_from_mixin, r_ordereddict
 
 from sast.exceptions import *
 
+def hashfn(exp):
+    return exp.hashfn()
+
+def hasheq(exp1, exp2):
+    return exp1.hashfn() == exp2.hashfn()
 
 class SastExp(object):
-    pass
+    def __init__(self):
+        pass
+    def hashfn(self):
+        return compute_identity_hash(self)
 
-class SastUndefined(SastExp):
-    pass
+class SastObject(SastExp):
+    def __init__(self):
+        self.od = r_ordereddict(hasheq, hashfn)
+
+class SastUndefined(SastObject):
+    def __init__(self):
+        pass
 
